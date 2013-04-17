af90de6c4f4105f917a60e9686833d95261a2506
Alex Hart <alex.hart658@gmail.com>
2013-04-17 09:38:41 -0300

    Add base driver, none of it's built out yet.

---
+++ src/ca/exallium/stem/db/Driver.java
@@ -0,0 +1,33 @@
+package ca.exallium.stem.db;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * SQL Driver
+ */
+public class Driver extends SQLiteOpenHelper {
+
+    public Driver(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    public int update(String sql) {
+        Cursor c = getWritableDatabase().rawQuery(sql, null);
+        return c.getCount();
+    }
+
+    public Cursor query(String sql) {
+        return getReadableDatabase().rawQuery(sql, null);
+    }
+}
